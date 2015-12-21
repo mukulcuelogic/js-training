1865f3c374193edeecefe0af7bd3c71b8fa3207d
Cuelogic <cuelogic@Cuelogics-Mac-mini-2.local>
2015-12-21 20:20:11 +0530

    push changes

--- day1/day1_simple.js
+++ day1/day1_simple.js
@@ -1,70 +1,35 @@
 //// simple way
             //init
-            function createNewVariable(type) {
-                
-                switch(type) {
-                    
-                    case "int":
-                        var newVar = new Number();
-                    break;
+var var_int = 0;
+var var_string = "";
+var var_bool = false;
+var var_array = [];
+var var_object = {};
 
-                    case "string":
-                        var newVar = new String();
-                    break;
+//add value into variable
+function addValue(newVar , value) {
+    return newVar = value;
+}
 
-                    case "boolean":
-                         var newVar = new Boolean();
-                    break;
+//push value into array
+function pushValueIntoArray(value) {
+    return var_array.push(value);
+}
+//remove value from array
+function removeValuefromArray(value) {
+    var valuePos = var_array.indexOf(value);
+    var_array.splice(valuePos, 1); 
+}
+//toogle bool value
+function toogleBool() {
+    return var_bool = !var_bool;
+}
+// concat string
+function stringConcat(stringValue) {
 
-                    case "object":
-                        var newVar = new Object();
-                    break;
-
-                    case "array":
-                        var newVar = new Array();
-                    break;
-                }
-                
-                
-                return newVar;
-            }
-            //add value into variable
-            function addValue(newVar , value) {
-                return newVar = value;
-            }
-            //push value into array
-            function pushValueIntoArray(newVar , value) {
-                return newVar.push(value);
-            }
-            //remove value from array
-            function removeValuefromArray(newVar , value) {
-                var valuePos = newVar.indexOf(value);
-                newVar.splice(valuePos, 1); 
-            }
-            //toogle bool value
-            function toogleBool(newVar) {
-                return newVar = !newVar;
-            }
-            // concat string
-            function stringConcat(newVar , stringValue) {
-
-               return newVar = newVar.concat(stringValue);
-            }
+//var_string = var_string + stringValue;
+   return var_string = var_string.concat(stringValue);
+}
             
             
-         var newBool = createNewVariable('boolean');
-         newBool = addValue(newBool , true);
-         newBool = toogleBool(newBool);
-         console.log(newBool);
          
-         var newArray = createNewVariable('array');
-         console.log(newArray);
-         
-         var newInt = createNewVariable('int');
-         console.log(newInt);
-         
-         var newString = createNewVariable('string');
-         console.log(newString);
-         
-         var newObject = createNewVariable('object');
-         console.log(newObject);
